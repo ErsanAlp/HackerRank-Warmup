9ced6013ea499716b5095ae08d1549b24b5152aa
ErsanAlp <ersanalp@sabanciuniv.edu>
2023-07-17 14:24:15 +0300

    Warmup changes On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   Warmup/A_Very_Big_Sum.cpp
    	new file:   Warmup/Birthday_Cake_Candles.cpp
    	new file:   Warmup/Compare_The_Triplets.cpp
    	new file:   Warmup/Diagonal_Difference.cpp
    	new file:   Warmup/Mini-Max_Sum.cpp
    	new file:   Warmup/Plus_Minus.cpp
    	new file:   Warmup/Solve_Me_First.cpp
    	new file:   Warmup/Staircase.cpp
    	new file:   Warmup/Time_Conversion.cpp

---
+++ Warmup/Solve_Me_First.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  if(1<=a && a<=1000 && 1<=b && b<=1000){
+      return a+b;
+  }
+  else{
+      return 0;
+  }
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
---
+++ Warmup/Time_Conversion.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    int hour = stoi(s.substr(0,2));
+    int min = stoi(s.substr(3,2));
+    int sec = stoi(s.substr(6,2));
+    string ns;
+    if (s.substr(8,1) == "P" && hour != 12) {
+        hour += 12;
+        string h = to_string(hour);
+        ns = h + ":" + s.substr(3,2) + ":" + s.substr(6,2);
+        return ns;   
+    }
+    else if (s.substr(8,1) == "A" && hour != 12) {
+        ns = s.substr(0,2) + ":" + s.substr(3,2) + ":" + s.substr(6,2);
+        return ns; 
+    }
+    else if (s.substr(8,1) == "P" && hour == 12) {
+        ns = s.substr(0,2) + ":" + s.substr(3,2) + ":" + s.substr(6,2);
+        return ns; 
+    }
+    else {
+        ns = "00:" + s.substr(3,2) + ":" + s.substr(6,2);
+        return ns; 
+    }     
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
