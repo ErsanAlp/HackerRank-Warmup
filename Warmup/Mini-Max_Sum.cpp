4d82e5d27879b32d3a000c2520965a82b3ded1ee
ErsanAlp <115344168+ErsanAlp@users.noreply.github.com>
2023-07-17 14:32:44 +0300

    Update Mini-Max_Sum.cpp

--- Warmup/Mini-Max_Sum.cpp
+++ Warmup/Mini-Max_Sum.cpp
@@ -1,3 +1,9 @@
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,12 +11,6 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
-
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
 
 void miniMaxSum(vector<int> arr) {
     int length = arr.size();
