61000d7e088ff51868306e2f745153eb976f38bf
ErsanAlp <115344168+ErsanAlp@users.noreply.github.com>
2023-07-17 14:31:18 +0300

    Update A_Very_Big_Sum.cpp

--- Warmup/A_Very_Big_Sum.cpp
+++ Warmup/A_Very_Big_Sum.cpp
@@ -1,3 +1,10 @@
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,12 +13,6 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'aVeryBigSum' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts LONG_INTEGER_ARRAY ar as parameter.
- */
 
 long aVeryBigSum(vector<long> ar) {
     long sum = 0;
